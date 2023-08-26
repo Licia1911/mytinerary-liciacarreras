8828361c3deacffb04933140428490f0ac6f288c
Licia <licia.carreras@gmail.com>
2023-08-25 21:11:35 -0300

    Front de Itinerary finalizado

---
+++ src/components/Activity.jsx
@@ -0,0 +1,8 @@
+
+export default function Activity() {
+    return (
+        <div className=" flex items-center justify-center mb-4">
+            <div className=" w-[750px] h-[550px] bg-cover bg-no-repeat flex items-center justify-center" style={{ backgroundImage: `url(../img/construction.jpg)` }}></div>
+        </div>
+    )
+}
