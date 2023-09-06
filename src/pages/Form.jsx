0f460fa16c9531b5362cb3add06018f7b78d30e2
Licia <licia.carreras@gmail.com>
2023-09-05 23:37:36 -0300

    Formulario

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 import apiUrl from '../apiUrl'
 import IconoAnimado from '../components/IconoAnimado'
@@ -7,14 +8,14 @@ import { Link as Anchor } from 'react-router-dom'
 
 export default function Form() {
 
-    const name = useRef()
-    const lastName = useRef()
-    const country = useRef()
-    const photo = useRef()
-    const mail = useRef()
-    const password = useRef()
+    //const [changeForm, setChangeForm] = useState(true)
 
-    const [changeForm, setChangeForm] = useState(true)
+    const name = useRef("")
+    const lastName = useRef("")
+    const country = useRef("")
+    const photo = useRef("")
+    const mail = useRef("")
+    const password = useRef("")
 
     async function handleSignUp() {
         try {
@@ -26,10 +27,12 @@ export default function Form() {
                 mail: mail.current.value,
                 password: password.current.value
             }
-            // await axios.post(
-            //     apiUrl + 'users/signup',
-            //     data
-            // )
+
+            await axios.post(
+                apiUrl + 'users/signup',
+                data
+            )
+
             console.log(data);
 
         } catch (error) {
@@ -37,14 +40,15 @@ export default function Form() {
         }
     }
 
+
     return (
         <>
-            <div className="flex flex-col h-[120vh] bg-cover bg-center justify-around items-center sm:flex-row" style={{ backgroundImage: `url(../img/backgroundLog.jpg)` }}>
-                <div className="w-[50%] flex items-center text-center justify-center sm:pl-20 pb-16">
-                    <h1 className="text-4xl mx-3 font-bold " >My Tinerary</h1>
-                    <IconoAnimado />
+            <div className="row flex h-[120vh] bg-cover bg-center justify-around items-center" style={{ backgroundImage: `url(../img/backgroundLog.jpg)` }}>
+                <div className="w-[50%] flex items-center pl-20 pb-16">
+                    <h1 className="text-4xl mx-3 font-bold" >My Tinerary</h1>
+
                 </div>
-                {changeForm ? <div className='flex  flex-col w-[360px] h-[500px] bg-neutral-50 p-6 '>
+                <div className='flex  flex-col w-[400px] h-[500px] bg-neutral-50 p-6 '>
                     <p className=' text-[12px] tetx-[#1C1C1C] pb-4'>step 1 of 2</p>
                     <h1 className=' text-[20px] items-start justify-center pb-4'>Create account</h1>
                     <div className='flex items-center justify-start '>
@@ -57,19 +61,16 @@ export default function Form() {
                         <div className=' w-[150px] text-[12px] border-b border-[#1c1c1c] h-[30px] m-2'></div>
                     </div>
                     <p className=' text-[12px] tetx-[#1C1C1C] font-semibold mt-4'>Sign up with email</p>
-                    <div className='flex items-center w-[400px] pt-2'>
-                        <p className='text-[12px] tetx-[#1C1C1C] p-1'>Already have an account?</p>
-                        <Anchor to={'/signin'} className=" text-[#4F46E5] hover:text-sky-600 flex text-[12px] ">Sign In </Anchor>
-                    </div>
-
+                    <p className=' text-[12px] tetx-[#1C1C1C] pb-2'>Already have an account? <span className='text-bold cursor-pointer text-blue-600 underline '>Sign in</span></p>
                     <form className=' text-[12px] mb-4'>
-                        <input ref={mail} type="text" className=' w-[320px] text-[12px] border-b border-[#1c1c1c] h-[60px]' name='mail' id='mail' placeholder='Email' />
-                        <input ref={password} type="password" className=' w-[320px] text-[12px] border-b border-[#1c1c1c] h-[60px] ' name='password' id='password' placeholder='Password' />
+                        <input ref={mail} type="text" className=' w-[346px] text-[12px] border-b border-[#1c1c1c] h-[60px]' name='mail' id='mail' placeholder='Email' />
+                        <input ref={password} type="password" className=' w-[346px] text-[12px] border-b border-[#1c1c1c] h-[60px]' name='password' id='password' placeholder='Password' />
                     </form>
                     <div className="flex h-[80px] items-end justify-end">
-                        <button className="rounded-md bg-[#4F46E5] hover:bg-sky-600 text-white text-[16px] text-center cursor-pointer w-[100px] h-13 p-1 mb-2 mr-2" onClick={() => setChangeForm(!changeForm)} >Continue </button>
+                        <div className="rounded-md bg-[#4F46E5] hover:bg-sky-600 text-white text-[16px] text-center cursor-pointer w-[100px] h-13 p-1 mb-2 mr-2"  >Continue </div>
                     </div>
-                </div> : <div className='flex  flex-col w-[400px] h-[500px] bg-neutral-50 p-6 '>
+                </div>
+                <div className='flex  flex-col w-[400px] h-[500px] bg-neutral-50 p-6 '>
                     <p className=' text-[12px] tetx-[#1C1C1C] pb-3'>step 2 of 2</p>
                     <h1 className=' text-[20px] items-start justify-center pb-3'>Create account</h1>
                     <p className=' text-[12px] tetx-[#1C1C1C] pb-2'>Already have an account? <span className='text-bold cursor-pointer text-blue-600 underline '>Sign in</span></p>
@@ -79,13 +80,13 @@ export default function Form() {
                         <input ref={photo} type="text" className=' w-[346px] text-[12px] border-b border-[#1c1c1c] h-[60px]' name='photo' id='photo' placeholder='Type Photo' />
                     </form>
                     <div className='flex items-center justify-start w-full mt-6'>
-                    <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                        <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
                         <select id="countries" className=" w-[166px] flex text-sm">
                             <option >Country/Region</option>
-                            <option  ref={country}>Argentina</option>
-                            <option  ref={country}>United States</option>
-                            <option  ref={country}>Canada</option>
-                            <option  ref={country}>France</option>
+                            <option ref={country}>United States</option>
+                            <option ref={country}>Canada</option>
+                            <option ref={country}>France</option>
+                            <option ref={country}>Germany</option>
                         </select>
                     </div>
                     <div className="flex">
@@ -99,9 +100,10 @@ export default function Form() {
                     <div>
                         <p className='text-[12px] tetx-[#1C1C1C] mt-4'>By clicking Create account, I agree that I have read and accepted the <span className='text-blue-600 underline cursor-pointer'>Terms of Use</span>Terms of Use and <span className='text-blue-600 underline cursor-pointer'>Privacy Policy</span>.</p>
                     </div>
-
-                    < input type='button' className="rounded-md bg-[#4F46E5] hover:bg-sky-600 text-white text-[16px] text-center cursor-pointer w-[100px] h-13 p-1 mb-2 mr-2 items-end justify-end" onClick={handleSignUp} value="Continue" />
-                </div>}
+                    <div className="flex h-[80px] items-end justify-end">
+                        <div className="rounded-md bg-[#4F46E5] hover:bg-sky-600 text-white text-[16px] text-center cursor-pointer w-[100px] h-13 p-1 mb-2 mr-2" onClick={handleSignUp}>Continue </div>
+                    </div>
+                </div>
 
             </div>
         </>
